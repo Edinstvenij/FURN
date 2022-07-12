554af0e67f56df72dab1f688cbc7016115cd5699
Yura <yuravigovskij@gmail.com>
2022-07-12 13:39:14 +0300

    Новая старница дизайнеров

--- designer.php
+++ designer.php
@@ -5,24 +5,63 @@ require_once 'blocks/header.php' ?>
 <!-- END header -->
 
 <!-- main -->
-<main class="designer">
-  <div class="designer__wrapper">
-    <div class="wrapper-img designer__wrapper-img">
-      <picture>
-        <!-- <source src="" type="image/webp"> -->
-        <source src="img/designer/1-designer.webp" type="image/webp">
-        <img src="img/designer/1-designer.png" alt="designer">
-      </picture>
-    </div>
-    <div class="wrapper-img designer__wrapper-img">
-      <picture>
-        <!-- <source src="" type="image/webp"> -->
-        <source src="img/designer/2-designer.webp" type="image/webp">
-        <img src="img/designer/2-designer.png" alt="designer">
-      </picture>
-    </div>
+<main id="top" class="wrapper-container-designer">
+  <section class="designer">
+    <div class="designer__wrapper">
+      <div class="wrapper-img designer__wrapper-item">
+        <picture>
+          <source media="(max-width: 500px)" src="img/designer/1-designer-mobile.webp" type="image/webp">
+          <source media="(min-width: 501px)" src="img/designer/1-designer.webp" type="image/webp">
+          <img class="designer__img" src="img/designer/1-designer.png" alt="designer">
+        </picture>
+        <div class="designer__description">
+          <div class="designer__description-background"></div>
+          <div class="designer__text">
+            <h2 class="designer__title">ANNA BAiERZDORF</h2>
+            <div class="designer__subtitle">
+              <p>And we are happy to present to you our designer products.</p>
+              <p>Our team develops interior items based on modern visual culture and unusual techniques that we widely use in our work.</p>
+              <p>We pay a lot of attention to detail and work with experienced professionals to ensure that the final product can please you for many years.</p>
+              <p>Get to know ukraine through people, through their work, and they will not leave you indifferent.</p>
+            </div>
+          </div>
+        </div>
 
-  </div>
+      </div>
+      <div class="wrapper-img designer__wrapper-item">
+        <picture>
+          <source media="(max-width: 500px)" src="img/designer/2-designer-mobile.webp" type="image/webp">
+          <source media="(min-width: 501px)" src="img/designer/2-designer.webp" type="image/webp">
+          <img class="designer__img" src="img/designer/2-designer.png" alt="designer">
+        </picture>
+        <div class="designer__description">
+          <div class="designer__description-background"></div>
+          <div class="designer__text">
+            <h2 class="designer__title">SERGEY BAiERZDORF</h2>
+            <div class="designer__subtitle">
+              <p>And we are happy to present to you our designer products.</p>
+              <p>Our team develops interior items based on modern visual culture and unusual techniques that we widely use in our work.</p>
+              <p>We pay a lot of attention to detail and work with experienced professionals to ensure that the final product can please you for many years.</p>
+              <p>Get to know ukraine through people, through their work, and they will not leave you indifferent.</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </section>
+  <section class="about">
+    <div class="about__text">
+      <h1 class="designer__title">HI!<br>WE ARE FÜRN</h1>
+      <div class="designer__subtitle">
+        <p>And we are happy to present to you our designer products.</p>
+        <p>Our team develops interior items based on modern visual culture and unusual techniques that we widely use in our work.</p>
+        <p>We pay a lot of attention to detail and work with experienced professionals to ensure that the final product can please you for many years.</p>
+        <p>Get to know ukraine through people, through their work, and they will not leave you indifferent.</p>
+      </div>
+    </div>
+  </section>
 </main>
 <!-- END main -->
 
