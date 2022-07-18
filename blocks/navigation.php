83e70a87d76d9af56f1b2b79226905635d3dd7af
Yura <yuravigovskij@gmail.com>
2022-07-18 14:02:22 +0300

    fix mobail

--- blocks/navigation.php
+++ blocks/navigation.php
@@ -3,7 +3,7 @@
   echo 'header-all-product__nav';
 } ?>">
   <div class="header__nav-close header__open-close"></div>
-  <ul>
+  <ul class="header__nav-ul">
     <li class="header__nav-li"><a href="all-product.php">Shop</a></li>
     <li class="header__nav-li"><a href="designer.php">Team</a></li>
     <li class="header__nav-li"><a href="delivery.php">Delivery</a></li>
